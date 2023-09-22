7724912f1c67870a0792d58fa0c5b37c92f074a8
Mayank Kamboj <mayankkamboj47@gmail.com>
2023-09-22 20:00:04 +0530

    update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'To Do App',
+  title: 'Pomodo',
   description: 'A todo and pomodoro app',
 }
 
